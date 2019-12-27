93f06a2dd39a4c497d59d680d10fa500a1c4bcdf
Anzhela Sagimbekova <anzhela.sagimbekova@aspekt.com.mk>
2019-12-27 10:02:09 +0600

    add HomeWork 41

---
+++ ItAcademy.sql
@@ -0,0 +1,72 @@
+﻿CREATE TABLE Contact
+(
+	ContactID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	FirstName VARCHAR(100) NOT NULL,
+	LastName VARCHAR(100) NOT NULL,
+	MiddleName VARCHAR(100),
+	BirthOfDate DATE NOT NULL,
+	IDCardNumber varchar(100) NOT NULL,
+	Gender VARCHAR(100)
+);
+
+CREATE TABLE Student
+(
+	StudentID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	ContactID INTEGER,
+	f_Active boolean NOT NULL,
+	FOREIGN KEY (ContactID) REFERENCES Contact(ContactID)
+);
+
+CREATE TABLE Course
+(
+	CourseID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	Name VARCHAR(100) NOT NULL,
+	Months int not null
+);
+
+CREATE TABLE Employee
+(
+	ContactID INTEGER NOT NULL PRIMARY KEY,
+	f_Active boolean NOT NULL
+);
+
+CREATE TABLE Event
+(
+	EventID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	Name varchar(100) NOT NULL,
+	OpeningDate DATE NOT NULL,
+	ClosingDate DATE
+);
+
+CREATE TABLE Teacher
+(
+	TeacherID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	CourseID int,
+	ContactID int,
+	Name varchar(100) not null,
+	FOREIGN KEY (CourseID) REFERENCES Course(CourseID),
+	FOREIGN KEY (ContactID) REFERENCES Contact(ContactID)
+);
+
+INSERT INTO Contact VALUES ('Ale', 'Aleeve', 'Alevna', '1996-05-04', 'AN2015', 'Female');
+INSERT INTO Contact(FIRSTName, LASTName, MiddleName, BirthOfDate, IDCardNumber, GEnder)
+VALUES ('Fedor', 'Fedor', 'Fedorovich', '2000-02-01', 'A5555', 'Male'),
+	   ('a', 'aa', 'aaa', '2000-02-01', 'A5554', 'Male'),
+	   ('bbb', 'bb', 'bbb', '2000-02-01', 'A5553', 'Male');
+INSERT INTO Employee VALUES (1, TRUE), (2, True);
+INSERT INTO Teacher VALUES(1, 1, 3, 'a');
+INSERT INTO Student VALUES(1, 4, TRUE);
+INSERT INTO Course VALUES (1, 'Programming', 9);
+
+ALTER TABLE Student ADD CourseID INT FOREIGN KEY REFERENCES Course(CourseID);
+
+UPDATE  Student SET  courseID = 1 WHERE  StudentID = 1;
+
+SELECT 
+	c.Name AS Course,
+	t.Name AS Teacher,
+	con.FirstName AS Student
+FROM Course c
+	INNER join Teacher t ON t.CourseID = c.CourseID
+	INNER join Student s ON s.CourseID = c.CourseID
+	INNER join Contact con ON con.ContactID = s.ContactID
---
+++ SQLQuery.sql
@@ -0,0 +1,53 @@
+﻿CREATE TABLE Visitor
+(
+	visitorID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	FirstName VARCHAR(100) NOT NULL,
+	LastName VARCHAR(100) NOT NULL,
+	MiddleName VARCHAR(100),
+	BirthOfDate DATE NOT NULL,
+	Gender VARCHAR(100)
+);
+
+INSERT INTO Visitor(FirstName, LastName, MiddleName, BirthOfDate, Gender) 
+	VALUES('A', 'James', 'A', '2001-01-03', 'Male');
+INSERT INTO Visitor(FirstName, LastName, MiddleName, BirthOfDate) 
+	VALUES('J', 'Bond', 'J', '2000-05-07');
+INSERT INTO Visitor(FirstName, LastName, BirthOfDate, Gender) 
+	VALUES('B', 'Alexa', '2002-01-05','Female');
+
+CREATE TABLE Visits
+(
+	visitID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	visitorID INT NOT NULL,
+	Name VARCHAR(100) NOT NULL,
+	VisitTime TIME NOT NULL,
+	Trainer VARCHAR(100) NOT NULL,
+	FOREIGN KEY (visitorID) REFERENCES Visitor(visitorID)
+);
+
+INSERT INTO Visits(visitorID, Name, VisitTime, Trainer)
+	VALUES (2, 'James', '13:23:44', 'B'), (3, 'James', '15:45:21', 'A');
+
+SELECT 
+	v.FirstName, 
+	v.LastName, 
+	v2.Trainer, 
+	v2.VisitTime 
+FROM Visitor v
+	INNER JOIN Visits v2 on v.visitorID = v2.visitorID
+ORDER BY v2.VisitTime DESC;
+
+ALTER TABLE Visits ADD sport_type VARCHAR(100);
+
+UPDATE Visits SET sport_type = 'Tenis' WHERE visitID = 2;
+UPDATE Visits SET sport_type = 'Swimming' WHERE visitID IN (1, 3);
+
+SELECT 
+	v.FirstName, 
+	v.LastName, 
+	v2.Trainer, 
+	v2.VisitTime 
+FROM Visitor v
+	INNER JOIN Visits v2 on v.visitorID = v2.visitorID
+WHERE sport_type LIKE 'Swim%'
+ORDER BY v2.VisitTime DESC;
