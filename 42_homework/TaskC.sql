498808d432fdb8af8422f0cc11b2e3932a5e81f3
Sagimbekova <pageofanzhe@gmail.com>
2020-01-18 18:38:38 +0600

    answer Hw 42

---
+++ 42_homework/TaskC.sql
@@ -0,0 +1,53 @@
+﻿CREATE TABLE people(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name varchar(50) NOT NULL UNIQUE,
+	birtdate date NOT NULL
+);
+
+CREATE TABLE countries(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	code int UNIQUE NOT NULL,
+	name varchar(50) NOT NULL UNIQUE,
+	population int NOT NULL,
+	president int REFERENCES people(id) NOT NULL
+);
+
+CREATE TABLE cities(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name varchar(50) NOT NULL,	
+	population int NOT NULL,
+	country_id int REFERENCES countries(id) NOT NULL,
+	mayor int REFERENCES people(id) NOT NULL
+);
+
+insert into people (name, birtdate) values
+	('Гурбангулы Бердымухамедов', '1957-05-29'),
+	('Владимир Путин', '1952-02-07'),
+	('Джонни Депп', '1974-11-11'),
+	('Брэд Питт', '1963-12-18'),
+	('Том Круз', '1962-07-02'),
+	('Николас Кейдж', '1964-01-07'),
+	('Роберт де Ниро', '1943-08-17'),
+	('Джек Николсон', '1937-07-07'),
+	('Кевин Костнер', '1955-01-18'),
+	('Том Хэнкс', '1956-07-09');
+
+insert into cities (name, population, country_id, mayor) values
+	('Moscow', 12506468, 1, 8), 
+	('Ashgabat', 1031992, 2, 5), 
+	('Saint Petersburg', 5351935, 1, 7),		
+	('Türkmenbaşy', 73000, 2, 9);
+
+insert into countries (code, name, population, president) values
+	(1, 'Russia', 146801931, 3), (2, 'Turkmenistan' ,5850901, 1);
+
+select  
+	ct.name as country, ci.name as city
+from countries ct
+	join cities ci on ct.id = ci.country_id
+where ct.name like 'T%' and ci.name like 'T%';
+
+select ct.name as country_name, sum(ci.population) as population
+from cities ci
+	join countries ct on ct.id = ci.country_id
+group by ct.name;
---
+++ 42_homework/TaskD.sql
@@ -0,0 +1,68 @@
+﻿CREATE TABLE students(
+	id serial PRIMARY KEY,
+	name varchar(50) NOT NULL,
+	birtdate date not null,
+	group_id int references groups(id)
+);
+
+insert into students (name, birtdate, group_id) values
+	('Гурбангулы Бердымухамедов', '1957-05-29', 1),
+	('Владимир Путин', '1952-02-07', 2),
+	('Джонни Депп', '1974-11-11', 3),
+	('Брэд Питт', '1963-12-18', 2),
+	('Том Круз', '1962-07-02', 1);
+
+CREATE TABLE groups(
+	id serial PRIMARY KEY,
+	code varchar(50) not null unique,
+	faculty_id int references faculties(id)
+);
+
+
+insert into groups (code, faculty_id) values ('IIT-1', 2), ('ET-1', 1), ('GI-1', 4) 
+
+CREATE TABLE faculties(
+	id serial PRIMARY KEY,
+	name varchar(50) not null unique,
+	university_id int references university(id)
+);
+
+insert into faculties (name, university_id) values ('Econom', 1), ('FIT', 1), ('International relationships', 2), ('Gum', 2)
+
+CREATE TABLE university(
+	id serial PRIMARY KEY,
+	name varchar(50) not null unique
+);
+
+alter table university add column student_id int references students(id);
+update university set student_id = 5 where name = 'KGTU';
+insert into university (name) values ('KEU'), ('KRAO'), ('KNU');
+
+CREATE TABLE subjects(
+	id serial PRIMARY KEY,
+	name varchar(50) not null unique
+);
+
+insert into subjects (name) values ('Database'), ('Mat analysis'), ('Networks'), ('Web');
+
+CREATE TABLE evaluations(
+	id serial PRIMARY KEY,
+	student_id int references students(id),
+	subject_id int references subjects(id),
+	evaluation int not null
+);
+
+insert into evaluations (student_id, subject_id, evaluation) values (1, 3, 5),(2, 3, 3),(3, 3, 4),(4, 3, 5),(5, 3, 4);
+
+select 
+	s.name as student, 
+	g.code as group, 
+	u.name as univetsity, 
+	f.name as faculty, 
+	avg(evaluation) as avgMark
+from evaluations e
+	join students s on e.student_id = s.id
+	join groups g on g.id = s.group_id
+	join university u on u.student_id = s.id
+	join faculties f on f.university_id = u.id
+group by s.name, g.code, u.name, f.name;
